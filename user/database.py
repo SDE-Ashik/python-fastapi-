b044e24305b0fba5e928dab6838a710f63bce06f
ashik <ashiashik5048@gmail.com>
2025-05-24 17:26:51 +0530

    user module created

--- user/database.py
+++ user/database.py
@@ -1,38 +1,38 @@
 from typing import List
 from uuid import uuid4
-from user.models.user_model import UserBase,User,UserCreate,UserUpdate
+from user.models.user_model import User, UserCreate, UserUpdate
 
-user_db :List[User]
+# Initialize the in-memory database
+user_db: List[User] = []
 
-
-def createUser(user:UserCreate)->User:
-    user_dct = User.dict()
+def createUser(user: UserCreate) -> User:
+    user_dct = user.dict()
     user_dct["id"] = str(uuid4())
-    user_obj = user(**user_dct)
+    user_obj = User(**user_dct)
     user_db.append(user_obj)
+    return user_obj
+
+def getUsers() -> List[User]:
     return user_db
-def getUsers()->List[User]:
-    return user_db
-def getUser(id:str)->User:
+
+def getUser(id: str) -> User:
     for user in user_db:
-      if  user.id ==id:
-          return user
+        if user.id == id:
+            return user
     return None
 
-def updateUser(id:str, updateUser:UserUpdate)->User:
+def updateUser(id: str, updateUser: UserUpdate) -> User:
     for user in user_db:
         if user.id == id:
             user.name = updateUser.name
             user.email = updateUser.email
             user.address = updateUser.address
-            return  user
-        return None
+            return user
+    return None
 
-def deleteUser(id:str)->bool:
-    for index, user in  enumerate(user_db):
+def deleteUser(id: str) -> bool:
+    for index, user in enumerate(user_db):
         if user.id == id:
             user_db.pop(index)
             return True
-    return  False
-
-
+    return False
--- user/models/user_model.py
+++ user/models/user_model.py
@@ -1,20 +1,19 @@
 from pydantic import BaseModel
 from typing import Optional
 
-
 class UserBase(BaseModel):
     name: str
     email: str
     address: Optional[str] = None
 
-
 class UserCreate(UserBase):
     pass
-
 
 class UserUpdate(UserBase):
     pass
 
-
 class User(UserBase):
     id: str
+
+    class Config:
+        from_attributes = True  # Enables Pydantic to work with ORM-like objects
